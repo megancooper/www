e0e8da7cf2559d02187830f32ba31b8e32c6b6a9
Megan Cooper <megancooper1000@gmail.com>
2022-09-23 23:40:43 -0700

    First Commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Megan Cooper";
+export const SITE_DESCRIPTION = "Personal site for Megan Cooper.";

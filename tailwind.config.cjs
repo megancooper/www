c882e9f52f19dbcee61f8f37744b796a38f87362
Megan Cooper <megancooper1000@gmail.com>
2023-05-11 18:56:46 -0700

    feat: update styles and add resume

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      title: ["Fjalla One", "sans-serif"],
+      title: ["IBM Plex Sans", "sans-serif"],
     },
     extend: {},
   },

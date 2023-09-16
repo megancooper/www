429c6690ee62def9703d5e9821cc571a67e7e3f8
Megan Cooper <megancooper1000@gmail.com>
2023-09-16 12:19:54 -0700

    feat: astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), image()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
